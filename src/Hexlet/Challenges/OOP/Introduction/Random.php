8f772b68d1e0615992955891c45cd6de7a7d103e
jPee2k <kipishept@gmail.com>
2020-05-31 12:15:40 +0300

    class Random implemented

---
+++ src/Hexlet/Challenges/OOP/Introduction/Random.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Home\Work\Hexlet\Challenges\OOP\Introduction;
+
+/**
+ * Генератор случайных чисел
+ * https://ru.hexlet.io/challenges/php_introduction_to_oop_rand
+ * 
+ * Random.php
+ * Реализуйте генератор рандомных чисел, представленный классом Random. 
+ * Интерфейс объекта включает в себя три функции:
+
+ * Конструктор. Принимает на вход seed, начальное число генератора 
+ * псевдослучайных чисел
+ *      getNext — метод, возврающающий новое случайное число
+ *      reset — метод, сбрасывающий генератор на начальное значение
+ * <?php
+
+ * $seq = new Random(100);
+ * $result1 = $seq->getNext();
+ * $result2 = $seq->getNext();
+
+ * $result1 != $result2; // true
+
+ * $seq->reset();
+
+ * $result21 = $seq->getNext();
+ * $result22 = $seq->getNext();
+
+ * $result1 === $result21; // true
+ * $result2 === $result22; // true
+ * Простейший способ реализовать случайные числа — линейный конгруэнтный метод.
+ * https://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D0%BD%D0%B5%D0%B9%D0%BD%D1%8B%D0%B9_%D0%BA%D0%BE%D0%BD%D0%B3%D1%80%D1%83%D1%8D%D0%BD%D1%82%D0%BD%D1%8B%D0%B9_%D0%BC%D0%B5%D1%82%D0%BE%D0%B4
+ */
+
+class Random
+{
+    const a = 37;
+    const c = 1;
+    const m = 99;
+
+    private $seed;
+    private $next;
+
+    function __construct(int $seed)
+    {
+        $this->seed = $seed;
+        $this->next = $this->seed;
+    }
+
+    public function getNext()
+    {
+        $this->next = (self::a * $this->next + self::c) % self::m;
+        return $this->next;
+    }
+
+    public function reset()
+    {
+        return $this->next = $this->seed;
+    }
+}
--- src/Hexlet/Challenges/OOP/Introduction/call.php
+++ src/Hexlet/Challenges/OOP/Introduction/call.php
@@ -1,9 +1,34 @@
 <?php
 
 use Home\Work\Hexlet\Challenges\OOP\Introduction\Circle;
+use Home\Work\Hexlet\Challenges\OOP\Introduction\Random;
 
 require __DIR__ . '/../../../../../vendor/autoload.php';
 
-$circle = new Circle(16);
-print_r($circle->getArea() . PHP_EOL);
-print_r($circle->getCircumferrence() . PHP_EOL);
+// $circle = new Circle(16);
+// print_r($circle->getArea() . PHP_EOL);
+// print_r($circle->getCircumferrence() . PHP_EOL);
+
+// ---------------------------------------------------------------- //
+
+// $random = new Random(5);
+// print_r($result1 = $random->getNext() . PHP_EOL);
+// print_r($result2 = $random->getNext() . PHP_EOL);
+
+// var_dump($result1 != $result2);
+
+// $random->reset();
+
+// print_r($result21 = $random->getNext() . PHP_EOL);
+// print_r($result22 = $random->getNext() . PHP_EOL);
+
+// var_dump($result1 === $result21);
+// var_dump($result2 === $result22);
+
+// $i = 20;
+// while ($i != 0) {
+//     echo $random->getNext() . PHP_EOL;
+//     $i--;
+// }
+
+// ---------------------------------------------------------------- //
