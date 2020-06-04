d2cb4ab8e322b3a20ce31af2342e2422341bfbec
jPee2k <kipishept@gmail.com>
2020-06-04 10:03:18 +0300

    class DeckOfCards / use tightenco-collect

---
+++ src/Hexlet/OOP/DeckOfCards.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Home\Work\Hexlet\OOP;
+
+use Tightenco\Collect\Support;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+class DeckOfCards
+{
+    private $cards;
+
+    public function __construct(array $cards)
+    {
+        $this->cards = collect($cards)->unique()->map(function ($card, $key) {
+                return array_fill(0, 4, $card);
+            })->collapse();
+    }
+
+    public function getShuffled()
+    {
+        return $this->cards->shuffle()->all();
+    }
+}
