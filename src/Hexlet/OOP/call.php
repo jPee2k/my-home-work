093a239080019fc3a3ce48557af5f3faaffedc70
jPee2k <kipishept@gmail.com>
2020-06-04 10:05:44 +0300

    update requires

--- src/Hexlet/OOP/call.php
+++ src/Hexlet/OOP/call.php
@@ -2,10 +2,13 @@
 
 use Home\Work\Hexlet\OOP\PasswordValidator;
 use Home\Work\Hexlet\OOP\Truncater;
+use Home\Work\Hexlet\OOP\DeckOfCards;
+
+use function Home\Work\Hexlet\OOP\Normalize\Normalize;
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
-//$debug = new PasswordValidator(['containNumbers' => true]);
+// $debug = new PasswordValidator(['containNumbers' => true]);
 // $password = 'qwerty';
 // print_r($debug->validate($password));
 
@@ -33,6 +36,44 @@ require __DIR__ . '/../../../vendor/autoload.php';
  * echo PHP_EOL;
  * print_r($truncater->truncate('one two', ['length' => 7]));
  * echo PHP_EOL;
+*/
+
+// ************************************************************** //
+
+// $cards = new DeckOfCards([1, 2, 2, 4, 8]);
+// print_r($cards->getShuffled());
+
+// ************************************************************** //
+
+/*
+ * $raw = [
+ *     [
+ *         'name' => 'istambul',
+ *         'country' => 'turkey'
+ *     ],
+ *     [
+ *         'name' => 'Moscow ',
+ *         'country' => ' Russia'
+ *     ],
+ *     [
+ *         'name' => 'iStambul',
+ *         'country' => 'tUrkey'
+ *     ],
+ *     [
+ *         'name' => 'antalia',
+ *         'country' => 'turkeY '
+ *     ],
+ *     [
+ *         'name' => 'samarA',
+ *         'country' => '  ruSsiA'
+ *     ],
+ *     [
+ *         'name' => 'istambul',
+ *         'country' => 'usa'
+ *     ],
+ * ];
+
+ * print_r(normalize($raw));
 */
 
 // ************************************************************** //
