5fd8b0d94cb461f4b672b79f4858223196c91948
jPee2k <kipishept@gmail.com>
2020-06-04 10:04:35 +0300

    Fluent Interface

---
+++ src/Hexlet/OOP/Normalize.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Home\Work\Hexlet\OOP\Normalize;
+
+use Tightenco\Collect\Support;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+/**
+ * Fluent Interface
+ * https://ru.hexlet.io/courses/php-object-oriented-design/lessons/fluent-interface/exercise_unit
+ */
+
+function normalize(array $assoc)
+{
+    $collection = collect($assoc);
+
+    return $collection->map(fn($items) => array_map(fn($item) => trim(strtolower($item)), $items))
+        ->unique(fn($item) => $item['name'] . $item['country'])
+        ->sortBy(fn ($city) => $city['name'])
+        ->mapToGroups(fn ($item) => [$item['country'] => $item['name']])
+        ->sortKeys()
+        ->toArray();
+}
