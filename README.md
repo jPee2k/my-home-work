4b1d4810400cb43ea6490d22cb556de0f9fb148c
jPee2k <kipishept@gmail.com>
2020-09-21 11:51:42 +0300

    added eslint, prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # my-home-work
+
 This repo was created for storing my homework.
 
 It contains some homework from https://ru.hexlet.io/ performed on offline IDE.
--- src/Hexlet/JS/Setup/index.js
+++ src/Hexlet/JS/Setup/index.js
@@ -1,4 +1,12 @@
-import _ from "lodash";
+/* eslint-disable no-console */
+import _ from 'lodash';
 
 console.log('Hello, World!');
 console.log(_.last(['one', 'two', 18]));
+
+const getName = (value) => {
+  const clearValue = value.trim();
+  return clearValue;
+};
+
+console.log(getName('John  '));
