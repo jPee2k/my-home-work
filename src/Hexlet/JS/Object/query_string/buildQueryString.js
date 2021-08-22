e2ea0b409b9721c624d180adda7b09907a64b2cd
Serhii Pylypenko <jpee.dev@gmail.com>
2021-08-22 21:38:04 +0300

    pass

--- src/Hexlet/JS/Object/query_string/buildQueryString.js
+++ src/Hexlet/JS/Object/query_string/buildQueryString.js
@@ -2,7 +2,11 @@
 /* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
 
 // BEGIN (write your solution here)
-export default () => {
-
+export default (params) => {
+  const entries = Object.entries(params);
+  return [...entries]
+    .sort(([a], [b]) => a.localeCompare(b))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
 };
 // END
