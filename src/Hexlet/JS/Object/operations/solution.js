562ceca44fc02e2fdaba2139d45acb1bdd036df9
Serhii Pylypenko <jpee.dev@gmail.com>
2021-08-22 21:15:53 +0300

    pass

--- src/Hexlet/JS/Object/operations/solution.js
+++ src/Hexlet/JS/Object/operations/solution.js
@@ -4,6 +4,25 @@
 import _ from 'lodash';
 
 // BEGIN (write your solution here)
-export default () => {
+export default (data1, data2) => {
+  const unionKeys = _.union(Object.keys(data1), Object.keys(data2));
+  const result = unionKeys.map((key) => {
+    if (!_.has(data1, key)) {
+      return [key, 'added'];
+    }
+
+    if (!_.has(data2, key)) {
+      return [key, 'deleted'];
+    }
+
+    if (!_.isEqual(data1[key], data2[key])) {
+      return [key, 'changed'];
+    }
+
+    return [key, 'unchanged'];
+  });
+
+  return Object.fromEntries(result);
 };
+
 // END
