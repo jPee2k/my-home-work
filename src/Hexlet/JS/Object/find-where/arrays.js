463158aec6250d27eefdadf33423d789f00f8382
Serhii Pylypenko <jpee.dev@gmail.com>
2021-08-22 20:46:59 +0300

    pass

--- src/Hexlet/JS/Object/find-where/arrays.js
+++ src/Hexlet/JS/Object/find-where/arrays.js
@@ -1,5 +1,13 @@
 // @ts-check
 /* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
 // BEGIN (write your solution here)
-export default () => {};
+export default (coll, term) => {
+  const entries = Object.entries(term);
+
+  const result = entries.reduce((acc, [key, value]) => (
+    acc.filter((item) => item[key] === value)
+  ), coll);
+
+  return result.length ? result[0] : null;
+};
 // END
